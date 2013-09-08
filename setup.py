9c49bc9e0c1bd5919dde94c4b4b12932ff7fc8b1
nvbn <nvbn.rm@gmail.com>
2013-09-08 10:59:38 +0400

    Add ci

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(
     name='coviolations_app',
     version=version,
-    description="",
-    long_description="""\
-""",
+    description="coviolations.io app",
+    long_description=open('README.rst').read(),
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
+    author='Vladimir Iakovlev',
+    author_email='nvbn.rm@gmail.com',
+    url='http://coviolations.io',
+    license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
